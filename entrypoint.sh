baa3bf5827e3ae147926b8a1ab56722044a93035
Gabe Cook <gabe565@gmail.com>
2023-09-15 00:54:34 -0500

    feat: Add multiple public IP handlers

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,24 +3,40 @@ set -eu
 
 CONFIGMAP_NAME="${CONFIGMAP_NAME:-external-dns-dynamic-ip}"
 DEPLOYMENT_NAME="${DEPLOYMENT_NAME:-external-dns}"
+HANDLERS="${HANDLERS:-cloudflare opendns ipinfo}"
 
-_get_ip() (
-  set -e
+_get_ip_cloudflare() (
   ip="$(dig +short txt ch whoami.cloudflare @1.1.1.1)"
   echo "${ip//\"/}"
+)
+
+_get_ip_opendns() (
+  dig +short myip.opendns.com @resolver1.opendns.com
+)
+
+_get_ip_ipinfo() (
+  wget -O- -q https://ipinfo.io/ip
 )
 
 _get_configmap_ip() (
   kubectl get configmap "$CONFIGMAP_NAME" --output=jsonpath='{.data.ip}' || true
 )
 
-ip="$(_get_ip)"
-configmap_ip="$(_get_configmap_ip)"
-
+for handler in $HANDLERS; do
+    ip="$("_get_ip_$handler" || true)"
+    if [[ -n "$ip" ]]; then
+        echo "Got IP from handler: $handler"
+        break
+    fi
+    echo "Failed to get public IP from handler: $handler"
+done
 if [[ -z "$ip" ]]; then
-  echo "Failed to get public IP: Empty response"
+  echo "All handlers failed"
   exit 1
-elif [[ "$ip" != "$configmap_ip" ]]; then
+fi
+
+configmap_ip="$(_get_configmap_ip)"
+if [[ "$ip" != "$configmap_ip" ]]; then
   echo "Patching configmap/$CONFIGMAP_NAME with new IP: $configmap_ip to $ip" >&2
 
   kubectl create configmap "$CONFIGMAP_NAME" \
